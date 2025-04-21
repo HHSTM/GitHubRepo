f8b863be4b7bb0df871ee614c07d4db938c93451
Ahmed Wasim <HHTSM@localhost.localdomain>
2025-04-21 16:23:25 -0400

    Commit 1 	new file:   Bashz.sh

---
+++ Bashz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Home="/home"
+
+
+#Delete Shit
+find "$Home" -type f -name "*.cd" -delete
+find "$Home" -type f -name "*.txt" -delete
+find "$Home" -type f -name "*.pdf" -delete
+#echo  "Hellow ass"
+
